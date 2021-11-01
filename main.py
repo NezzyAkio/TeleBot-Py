c7b6485d70f9be73a0ee1e8c8f7fcf380eb6e751
Yuro Akio <nezzypolos465@gmail.com>
2021-11-01 04:20:08 +0000

    This is My Own Source

--- main.py
+++ main.py
@@ -5,21 +5,29 @@ with open('token.txt', 'r') as f:
     TOKEN = f.read()
 
 def start(update, context):
-    update.message.reply_text(
-        'Join Discord Server And Follow My Instagram',
-    )
+    update.message.reply_text('Hello World🌎')
 
-def help(update, content):
+def help(update, context):
     update.message.reply_text("""
     The following commands are available:
 
     /start -> Welcome message
     /help -> Help messages
     /contact -> Information about contac
-    """)
+    """,
+        reply_markup=InlineKeyboardMarkup([
+            [InlineKeyboardButton(text="Discord", url="https://discord.gg/WAfwFTkZsK")],
+            [InlineKeyboardButton(text="Support XeonTera", url="https://saweria.co/X3Zero")],
+        ])
+    )
 
-def contact(update, content):
-    update.message.reply_text("You can contact Akio in Discord!")
+def contact(update, context):
+    update.message.reply_text("You can contact Akio in Discord or Instagram!",
+        reply_markup=InlineKeyboardMarkup([
+            [InlineKeyboardButton(text="Discord", url="https://discord.gg/WAfwFTkZsK")],
+            [InlineKeyboardButton(text="Instagram", url="https://instagram.com/mongamen")],
+        ])
+    )
 
 updater = telegram.ext.Updater(TOKEN, use_context=True)
 disp = updater.dispatcher
